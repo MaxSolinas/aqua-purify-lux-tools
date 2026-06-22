2489ed26b41e72d00be5912dd3942a4111dddcbf
Maxime Solinas <maxime.solinas@gmail.com>
2026-06-22 12:47:34 -0400

    Ajoute le lien de fiche technique aux produits

--- src/data/types.ts
+++ src/data/types.ts
@@ -11,39 +11,5 @@ export interface Produit {
     dims: string;
     pressure: string;
     temp: string;
-}
-
-export interface CommuneData {
-    th: number;
-    city?: string;
-    localities?: string[];
-}
-
-export interface MasterData {
-    [key: string]: CommuneData;
-}
-
-export interface Config {
-    contact: {
-        address: string;
-        phone: string;
-        web: string;
-        email: string;
-    };
-    apiGeoUrl: string;
-    constants: {
-        vrPerApt: number;
-        coefA: number;
-        coefB: number;
-        coefC: number;
-        consumptionPerPerson: number;
-        personsPerApt: number;
-    };
-}
-
-export interface SearchItem {
-    displayName: string;
-    searchName: string;
-    commune: string;
-    th: number;
+    link?: string;
 }
